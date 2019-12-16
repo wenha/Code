1d5c55cd9baa9b6ce6373edea518f89d5e8dd9a8
姚文豪 <yaowh@etlchina.net>
2019-12-16 19:52:13 +0800

    添加异步方法

---
+++ NETCORE/Demo/AsyncDemo/DemoTask/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OpenMainsSwithch();
+
+            Console.ReadLine();
+        }
+
+        public static int GetThreadId()
+        {
+            return Thread.CurrentThread.ManagedThreadId;
+        }
+
+        public static void OpenMainsSwithch()
+        {
+            Console.WriteLine("我和老婆在看电视，线程Id为{0}", GetThreadId());
+            Console.WriteLine("突然停电了，快去看看是不是跳闸了");
+            Task task = CommandOpenMainsSwithch();
+            Console.WriteLine("没电了先玩会手机吧，线程Id为{0}", GetThreadId());
+            Thread.Sleep(1000);
+            Console.WriteLine("手机也没电了只等电源打开，线程Id为{0}", GetThreadId());
+
+            while (!task.IsCompleted)
+            {
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("又有电了我们继续看电视，线程Id为{0}", GetThreadId());
+        }
+
+        public static async Task CommandOpenMainsSwithch()
+        {
+            Console.WriteLine("这时我准备去打开电源开关，线程Id为{0}", GetThreadId());
+            await Task.Run(() =>
+            {
+                Console.WriteLine("屁颠屁颠的去打开电源开关，线程Id为{0}", GetThreadId());
+                Thread.Sleep(10000);
+            });
+
+            Console.WriteLine("电源开关打开了，线程Id为{0}", GetThreadId());
+        }
+    }
+}
---
+++ NETCORE/Demo/AsyncDemo/DemoVoid/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoVoid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CookDinner();
+            Console.ReadLine();
+        }
+
+        public static int GetThreadId()
+        {
+            return Thread.CurrentThread.ManagedThreadId;
+        }
+
+        public static void CookDinner()
+        {
+            Console.WriteLine("老婆开始打扫房间了，线程为{0}", GetThreadId());
+            Console.WriteLine("垃圾满了，快去仍垃圾");
+            CommandDropLitter();
+            Console.WriteLine("不管他继续打扫，线程Id为{0}", GetThreadId());
+            Thread.Sleep(1000);
+            Console.WriteLine("老婆把房间打扫好了，线程Id为{0}", GetThreadId());
+        }
+
+        public static async void CommandDropLitter()
+        {
+            Console.WriteLine("这时我准备去扔垃圾，线程为{0}", GetThreadId());
+            await Task.Run(() =>
+            {
+                Console.WriteLine("屁颠屁颠的去扔垃圾，线程Id为{0}", GetThreadId());
+                Thread.Sleep(1000);
+            });
+
+            Console.WriteLine("垃圾扔了还有啥吩咐的，线程Id为{0}", GetThreadId());
+        }
+    }
+}
